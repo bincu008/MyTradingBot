b73a283354799084a26436a6a3a7593e72013ed1
Hieu <bincu008@gmail.com>
2024-09-10 21:34:59 +0700

    fix timing

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ import OrderRequest as OR
 import Logger
 
 refresh_train_data = False
-polling_time = 10 #seconds
-suspend_time = 10 #seconds
-trade_waiting_time = 10
+polling_time = 120 #seconds
+suspend_time = 300 #seconds
+trade_waiting_time = 120
 train_data_output_file = "train_data.csv"
 test_data_output_file = "test_data.csv"
 
